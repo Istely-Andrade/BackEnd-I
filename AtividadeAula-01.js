e405d95fe8098a441ae8da13f95b9688db8a4e13
Istely-Andrade <istelydasilva883@gmail.com>
2025-04-03 17:47:51 -0400

    Atividade 01

---
+++ AtividadeAula-01.js
@@ -0,0 +1,19 @@
+//Escreva um programa que declare duas variáveis, “nome” e “idade”, e as
+//imprima em um console em uma frase que diga “Olá, meu nome é [nome]
+//e eu tenho [idade] anos”.
+let nome = "Istely";
+let idade = "16";
+console.log("Olá, meu nome é " + nome + " e eu tenho " + idade + " anos.")
+
+//Declare uma string e utilize métodos para converter para maiúsculas e minúsculas
+let string = "Hello Word"
+console.log(string.toLowerCase());
+
+//Declare uma variável e verifique se o tipo dela é number.
+let valor = 14;
+console.log(typeof valor);
+
+//Calcule o Índice de Massa Corporal (IMC) utilizando variáveis para altura e peso.
+let altura = 1.60;
+let peso = 50;
+console.log((peso / (altura * altura)).toFixed(2));
--- Aula-01.js
+++ Aula-01.js
@@ -2,6 +2,28 @@ let valor = 13.141516;
 let text = "Meu primeiro projeto em JavaScript";
 let ativo = true;
 
-console.log(valor.toFixed(2));
-console.log(text.toUpperCase());
-console.log(ativo.toString());
+console.log(valor.toFixed(2));//Arredonda o valor para duas casas decimais
+console.log(typeof valor);//Retorna o tipo da variavel
+console.log(text.toUpperCase());//Converte o texto para maiusculo
+console.log(typeof text);//Retorna o tipo da variavel
+console.log(ativo.toString());//Converte para texto
+console.log(typeof ativo);//Retorna o tipo da varivel
+
+let estados = ["São Paulo","Minas Gerais","Rondônia","Acre"];
+let marcasCarros = [];
+marcasCarros.push("Toyota");
+console.log(estados[2]);
+console.log("Tamanho:" + estados.length);
+console.log("Total letras:" + marcasCarros[0].length);
+
+let usuario;//variavel não definida
+
+//let idade;
+//console.log(idade +1);//NaN
+
+let nula = null;
+//console.log(nula.length);
+console.log(nula + 1);
+
+const aula = "JavaScript";
+console.log(aula);
